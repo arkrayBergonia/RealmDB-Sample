599c024fb9c3fc3cb61a73cf2e54abe5150a6dde
arkray - Francis Bergonia <fbergonia.arkray@gmail.com>
2020-01-02 15:55:07 +0800

    add imageToLoad checker

--- SaveLoadImage/MainViewController.swift
+++ SaveLoadImage/MainViewController.swift
@@ -26,15 +26,28 @@ class MainViewController: UIViewController {
     }
 
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        self.imageToLoad.isHidden = !(self.retrieveImage(forKey: "keyImage", inStorageType: StorageType.fileSystem) != nil)
+        
+        if (self.retrieveImage(forKey: "keyImage", inStorageType: StorageType.userDefaults) != nil) {
+            self.imageToLoad.image = self.retrieveImage(forKey: "keyImage", inStorageType: StorageType.fileSystem)
+        }
+    }
+    
     @IBAction func saveBtnTapped(_ sender: Any) {
         self.selectFromGallery()
     }
     
     @IBAction func loadBtnTapped(_ sender: Any) {
         self.imageToLoad.isHidden = false
-        self.imageToLoad.image = self.retrieveImage(forKey: "keyImage", inStorageType: StorageType.userDefaults)
+        self.imageToLoad.image = self.retrieveImage(forKey: "keyImage", inStorageType: StorageType.fileSystem)
     }
     
+    func checkNoteImageDataIsAvailable(key: String) -> Bool {
+        return UserDefaults.standard.object(forKey: key) != nil
+    }
 }
 
 extension MainViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
@@ -52,7 +65,7 @@ extension MainViewController: UIImagePickerControllerDelegate, UINavigationContr
         guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else {fatalError("Expected a dictionary containing an image, but was provided the following: \(info)") }
         self.imageToSave.isHidden = false;
         self.imageToSave.image = selectedImage
-        self.store(image: selectedImage, forKey: "keyImage", withStorageType: StorageType.userDefaults)
+        self.store(image: selectedImage, forKey: "keyImage", withStorageType: StorageType.fileSystem)
         dismiss(animated: true, completion: nil)
     }
     

ee7feb33825555d27ced6c107c879e8bab68d612
arkray - Francis Bergonia <fbergonia.arkray@gmail.com>
2020-01-03 11:20:59 +0800

    Realm test save

--- SaveLoadImage/AppDelegate.swift
+++ SaveLoadImage/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RealmSwift
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,7 +16,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        print(Realm.Configuration.defaultConfiguration.fileURL)
+        
+        let memo = MemoData()
+        memo.memoText = "First entry to our journal"
+        do {
+            let realm = try Realm()
+            try realm.write {
+                realm.add(memo)
+            }
+        } catch {
+            print("Error in initializing new realm, \(error)")
+        }
+        
         return true
     }
 
---
+++ SaveLoadImage/Models/MemoData.swift
@@ -0,0 +1,15 @@
+//
+//  MemoData.swift
+//  SaveLoadImage
+//
+//  Created by Francis Jemuel Bergonia on 1/3/20.
+//  Copyright © 2020 Arkray PHM. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class MemoData: Object {
+    @objc dynamic var memoText: String = ""
+    @objc dynamic var memoImage: NSData? = nil
+}
